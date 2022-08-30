21e8d4aa39c47ad6d196660ea5fce70be9f3de30
Nikita <nikita.kras.kyiv@gmail.com>
2022-08-30 19:41:24 +0100

    The bug is present. Time Stamp for debugging

---
+++ DQN.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import wrappers
+from tf_agents.trajectories import time_step as ts
+from tf_agents.networks import network
+from tf_agents.specs import tensor_spec
+from tf_agents.policies import q_policy
+
+import Environment
+
+game = Environment.GridWorld(12)
+
+# Get information about the environment
+print(game.action_spec())
+print(game.map_spec())
+
+time_stamp = game.reset()
+wrapped_game = wrappers.TimeLimit(game, duration=50)
+tf_env = tf_py_environment.TFPyEnvironment(wrapped_game)
+
+print("***************************** TensorFlow Wrapper is applied ****************************")
+print(isinstance(tf_env, tf_environment.TFEnvironment))
+print("TimeStep Specs:", tf_env.time_step_spec())
+# print("Action Specs:", tf_env.action_spec())
+
+input_tensor_spec = tensor_spec.TensorSpec(shape=[12, 12, 1], dtype=tf.float32)
+time_step_spec = ts.time_step_spec(input_tensor_spec)
+
+print("New TimeStep Spec:", time_step_spec)
+
+# time_step_spec = tf_env.time_step_spec()
+# time_step_spec = tf.expand_dims(time_step_spec, axis=-1)        # To include place for batch (12,12) -> (12,12,)
+action_spec = tensor_spec.from_spec(tf_env.action_spec())
+num_actions = action_spec.maximum - action_spec.minimum + 1
+
+print("_________________________________")
+print("time_step_spec.observation:")
+print(time_step_spec.observation)
+print(time_step_spec.observation.shape.as_list())
+print("_________________________________")
+
+batch_size = 2
+# shape1 = batch_size
+# shapeList = time_step_spec.observation.shape.as_list()
+# shape2 = shapeList[0]
+# shape3 = shapeList[1]
+
+print("Shape: " + str([batch_size, 12, 12]))
+
+shape_of_element = tf_env.time_step_spec().observation.shape
+observation = tf.ones(shape=[batch_size, 12, 12])
+time_steps = ts.restart(observation, batch_size=batch_size)
+
+print("observation")
+print(observation)
+print("time_steps")
+print(time_steps)
+
+class QNetwork(network.Network):
+
+  def __init__(self, input_tensor_spec, action_spec, num_actions, name=None):
+    super(QNetwork, self).__init__(
+        input_tensor_spec=input_tensor_spec,
+        state_spec=(),
+        name=name)
+    self._sub_layers = [
+        tf.keras.layers.Dense(10),
+        tf.keras.layers.Dense(num_actions)
+    ]
+
+  def call(self, inputs, step_type=None, network_state=()):
+    del step_type
+    inputs = tf.cast(inputs, tf.float32)
+    for layer in self._sub_layers:
+      inputs = layer(inputs)
+    return inputs, network_state
+
+
+my_q_network = QNetwork(
+    input_tensor_spec=time_step_spec.observation,
+    action_spec=action_spec,
+    num_actions=num_actions)
+
+# print("----- Check Q-Network -------")
+# print(my_q_network)
+# my_q_network.__call__(observation)  # Important! To initialize weights and to set dimentions (shapes) in NN
+# print(my_q_network.summary())
+# print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+my_q_policy = q_policy.QPolicy(
+    time_step_spec=time_step_spec,
+    action_spec=action_spec,
+    q_network=my_q_network)
+
+
+action_step = my_q_policy.action(time_steps)
+distribution_step = my_q_policy.distribution(time_steps)
+
+print('Action:')
+print(action_step.action)
+
+print('Action distribution:')
+print(distribution_step.action)
--- Environment.py
+++ Environment.py
@@ -42,48 +42,6 @@ To use that environment do next:
 # To be wrapped by TensorFlow environment
 class GridWorld(py_environment.PyEnvironment):
 
-    # def __init__(self):
-    #     self._action_spec = array_spec.BoundedArraySpec(
-    #         shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
-    #     self._observation_spec = array_spec.BoundedArraySpec(
-    #         shape=(1,), dtype=np.int32, minimum=0, name='observation')
-    #     self._state = 0
-    #     self._episode_ended = False
-    #
-    # def action_spec(self):
-    #     return self._action_spec
-    #
-    # def observation_spec(self):
-    #     return self._observation_spec
-    #
-    # def _reset(self):
-    #     self._state = 0
-    #     self._episode_ended = False
-    #     return ts.restart(np.array([self._state], dtype=np.int32))
-    #
-    # def _step(self, action):
-    #
-    #     if self._episode_ended:
-    #         # The last action ended the episode. Ignore the current action and start
-    #         # a new episode.
-    #         return self.reset()
-    #
-    #     # Make sure episodes don't go on forever.
-    #     if action == 1:
-    #         self._episode_ended = True
-    #     elif action == 0:
-    #         new_card = np.random.randint(1, 11)
-    #         self._state += new_card
-    #     else:
-    #         raise ValueError('`action` should be 0 or 1.')
-    #
-    #     if self._episode_ended or self._state >= 21:
-    #         reward = self._state - 21 if self._state <= 21 else -21
-    #         return ts.termination(np.array([self._state], dtype=np.int32), reward)
-    #     else:
-    #         return ts.transition(
-    #             np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
-
     def __init__(self, size, goal_rewards=None, step_cost=-0.01, observation_step=5):
 
         self.action_space_size = 4
@@ -410,23 +368,17 @@ class GridWorld(py_environment.PyEnvironment):
                 else:
                     match self.state_matrix[row, col]:
                         # Wall
-                        case -1:
-                            row_string += ' # '
+                        case -1: row_string += ' # '
                         # Path
-                        case 0:
-                            row_string += ' - '
+                        case 0: row_string += ' - '
                         # Goal 1
-                        case 1:
-                            row_string += ' A '
+                        case 1: row_string += ' A '
                         # Goal 2
-                        case 2:
-                            row_string += ' B '
+                        case 2: row_string += ' B '
                         # Goal 3
-                        case 3:
-                            row_string += ' C '
+                        case 3: row_string += ' C '
                         # Goal 4
-                        case 4:
-                            row_string += ' D '
+                        case 4: row_string += ' D '
 
             row_string += '\n'
             graph += row_string
